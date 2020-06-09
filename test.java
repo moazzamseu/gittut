b218866fd7097acd0f5262ac2b8a496d20a36d56
Moazzam Sarkar <moazzamsarkarseu@gmail.com>
2020-06-09 14:52:21 +0600

    new file create

---
+++ test.java
@@ -0,0 +1,12 @@
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("Vs code is awesome");
+
+		greeting("Moazzam Sarkar")
+}
+	public static void greeting(String name) {
+		System.out.println("Welcome" + name);
+	}
+
+
+}
